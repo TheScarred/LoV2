490dbba8b5bbfb0541117ee8a59728b88ee5c7b4
HellAngeal <persia.2000@hotmail.com>
2019-12-02 15:49:57 -0600

    leaderboard

---
+++ Assets/Game/Networking/Scripts/LeaderBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.ObjectModel;
+
+public class LeaderBoard : MonoBehaviour
+{
+    public Image[] lugares;
+    public Text[] nombre;
+    public Text[] score;
+
+    void Score(List<LB> posiciones)
+    {
+        for(int i = 0; i < lugares.Length; i++)
+        {
+            if (posiciones.Count < lugares.Length)
+            {
+                nombre[i].text = posiciones[i].name;
+                score[i].text = posiciones[i].score.ToString();
+            }
+        }
+    }
+    struct LB
+    {
+        public string name;
+        public int score;
+    }
+}
